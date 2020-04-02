320e3c67ef1a27b14f3ab96b824e68bbe24594be
junko911 <junkotahara911@gmail.com>
2020-04-02 15:06:19 -0400

    Created the rest of the  methods

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,4 +21,11 @@ class Transfer
 			end
 		end
 	end
+	def reverse_transfer
+		if self.status == "complete"
+			self.sender.balance += self.amount
+			self.receiver.balance -= self.amount
+			self.status = "reversed"
+		end
+	end
 end
