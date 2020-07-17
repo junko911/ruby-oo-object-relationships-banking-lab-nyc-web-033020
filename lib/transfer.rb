a80f1cf9422e38ae261156bdacf1bae61364e832
junko911 <junkotahara911@gmail.com>
2020-07-16 21:47:54 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,31 +1,38 @@
+require 'pry'
+
 class Transfer
-	attr_accessor :sender, :receiver, :status, :amount
-	def initialize(sender,receiver,amount)
-		@sender = sender
-		@receiver = receiver 
-		@amount = amount
-		@status = 'pending'
-	end
-	def valid?
-		self.sender.valid? && self.receiver.valid?
-	end
-	def execute_transaction
-		if self.status != "complete"
-			if self.sender.balance < self.amount || !self.valid?
-				self.status = 'rejected'
-				"Transaction rejected. Please check your account balance."
-			else
-				self.sender.balance -= self.amount
-				self.receiver.balance += self.amount
-				self.status = "complete"
-			end
-		end
-	end
-	def reverse_transfer
-		if self.status == "complete"
-			self.sender.balance += self.amount
-			self.receiver.balance -= self.amount
-			self.status = "reversed"
-		end
-	end
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if status != "complete"
+      if sender.balance >= amount && valid?
+        self.sender.balance -= amount
+        self.receiver.balance += amount
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      self.receiver.balance -= amount
+      self.sender.balance += amount
+      self.status = "reversed"
+    end
+  end
 end
